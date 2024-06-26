a650b98ae2b49287cc74803e17059bb26dae028f
Akash-D-2003 <161223679+Akash-D-2003@users.noreply.github.com>
2024-06-26 19:33:54 +0530

    vivriti
    vivriti

---
+++ assets/js/main.js
@@ -0,0 +1,135 @@
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 400) {
+    $("header").addClass("sticky");
+  } else {
+    $("header").removeClass("sticky");
+  }
+});
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 900) {
+    $(".top-arrow").addClass("top-arrow-show ");
+  } else {
+    $(".top-arrow").removeClass("top-arrow-show ");
+  }
+});
+$(".loan-slider").slick({
+  infinite: true,
+  arrows: false,
+  dots: false,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 900,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
+$(".grow-background-img").slick({
+  dots: false,
+  arrows: false,
+  infinite: true,
+  speed: 500,
+  pauseOnHover: false,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  fade: true,
+  cssEase: "linear",
+});
+$(".ambition-slider").slick({
+  dots: false,
+  arrows: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+});
+$(".investors-slider").slick({
+  dots: false,
+  arrows: false,
+  autoplay: true,
+  autoplaySpeed: 0,
+  speed: 5000,
+  cssEase: "linear",
+  pauseOnHover: false,
+  slidesToShow: 6,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 900,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
+$(".board-directors-slick").slick({
+  arrows: true,
+  dots: false,
+  centerMode: true,
+  autoplay: true,
+  autoplaySpeed: 1000,
+  speed: 800,
+  cssEase: "linear",
+  centerPadding: "80px",
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 900,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
+$(".resources-slider").slick({
+  arrows: false,
+  dots: false,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 900,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
